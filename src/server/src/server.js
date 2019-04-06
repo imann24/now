2eaebb3df583a8307c2fc7358000a1c1c1bd5a5e
Isaiah Mann <isaiah@truex.com>
2019-04-06 02:23:40 -0700

    Correct client path in server

--- src/server/src/server.js
+++ src/server/src/server.js
@@ -3,24 +3,30 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+const clientDir = '../../client/build'
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 // API calls
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
 });
+
 app.post('/api/world', (req, res) => {
     console.log(req.body);
     res.send(
         `I received your POST request. This is what you sent me: ${req.body.post}`,
     );
 });
+
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'src/client/build')));
+    app.use(express.static(path.join(__dirname, clientDir)));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'src/client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, clientDir, 'index.html'));
     });
 }
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
