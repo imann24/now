9e8220deb39d2ecda81259b2e9c22bab77496637
Isaiah Mann <contact@isaiahmann.com>
2019-04-16 00:50:03 -0700

    Complete unit tests for socket.js client

--- src/client/src/socket.js
+++ src/client/src/socket.js
@@ -5,18 +5,12 @@ const CHAT_EVENT = 'chat';
 class Socket {
     constructor() {
         this.socket = io();
-        this.messageHandlers = [];
-        this.socket.on(CHAT_EVENT, (message) => {
-            this.messageHandlers.forEach(function(handler) {
-                handler(message);
-            });
-        });
     };
     sendMessage(message) {
         this.socket.emit(CHAT_EVENT, message);
     };
     subscribeToMessages(handler) {
-        this.messageHandlers.push(handler);
+        this.socket.on(CHAT_EVENT, handler);
     }
 }
 
